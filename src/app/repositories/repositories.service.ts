f56250d8509e8d3c839e1564ae6a752ec92521e9
Juan Carlos López <jcarloslopezl@hotmail.com>
2018-03-27 14:21:57 -0600

    Change github username
    Add form to select github username

--- src/app/repositories/repositories.service.ts
+++ src/app/repositories/repositories.service.ts
@@ -3,12 +3,12 @@ import { Http, Response} from '@angular/http';
 
 @Injectable()
 export class RepositoriesService {
-	
+
 	baseUrl: string = "https://api.github.com";
-	username: string = "jcarloslopezl";
+	username: string = "jclopezdev";
 
   constructor(private http: Http) { }
-  
+
 	getRepos(){
 		return this.http.get(this.baseUrl + "/users/" + this.username + '/repos');
 	}
